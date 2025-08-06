99277080f73ee99f778794e89cc9ab23f43aa817
Ermiyas Gera <ermiyasgera@gmail.com>
2025-08-06 11:41:03 -0800

    Add files via upload
    Basic Calculator Program

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# Basic Calculator Program
+# Get inputs from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+# Perform the calculation based on the operation
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is undefined.")
+else:
+    print("Invalid operation. Please enter one of +, -, *, /.")
